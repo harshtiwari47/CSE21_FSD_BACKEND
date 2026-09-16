68a594ed8d98fad356c6b4ca384e1e8ec7a3b287
harshtiwari47 <hrsri.x@gmail.com>
2026-09-16 06:30:13 +0530

    Add classtask/Day3.js

---
+++ classtask/Day3.js
@@ -0,0 +1,17 @@
+//Promises:object
+//Promise way 1
+//   const promiseOne=new Promise((resolve,reject)=>{
+//    console.log("promise done");
+//    resolve("operation successful");
+//    let success=true;
+//     if(success){
+//         resolve("operation successful");
+//     }else{
+//         reject("operation fail");
+//     }
+//   })
+//   promiseOne.then((result)=>{
+//  console.log(result);
+//   }).catch((error)=>{
+//   console.log(error);
+//   })
