e02cf70effd8c15db41988fd8b8e43f53e031bb5
harshtiwari47 <hrsri.x@gmail.com>
2026-09-16 06:30:13 +0530

    Add classtask/app.js

---
+++ classtask/app.js
@@ -0,0 +1,32 @@
+//function in js:block of code
+//syntax :
+//function fname(){
+//}
+//fname();
+// function add (num1, num2) {
+// console.log(num1 + num2);
+// }
+// add(2,1);
+// function add(num1, num2) {
+//     return num1 + num2;
+// }
+// add(2, 1);
+// //arrow function
+// //variable in js:container to store data
+// //var ,let,const
+// //syntax:()=>{}
+// const add=()=>{
+// console.log("arrow function")
+// }
+// add();
+// const add=(num1,num2)=>{
+//     return num1 + num2;
+// }
+// console.log(add(2,1));
+// arguments:array like object
+// function addNum(){
+//     console.log(arguments);
+
+// }
+// addNum(2,1,3,4,5,6,7,8,9,10);
+// //nodejs:runtime environment to run js code outside the browser
